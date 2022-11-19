a92cb89cad78ef3fc3b64f07053e0c3b3b6b3fa5
TK <thiratikan@gmail.com>
2022-11-19 02:25:13 -0500

    Update searchbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,27 +5,22 @@ function Home() {
   const [items, setItems] = useState([]);
   const [query, setQuery] = useState("");
   // query is for what to search for
+  const [filterData, setFilterData] = useState([]);
 
   const filterItems = useMemo(() => {
     // return an array of item that you return
-    return items.map((item) => {
-      console.log({
-        // spread operator (GIVE ME ALL THE ITEMS)
-        ...item,
-        "Common name": item["Common name"],
-        "Latin name": item["Latin name"],
-      });
+    return items.map((allName) => {
+      const {
+        // spread operator (GIVE ME ALL THE ITEMS)...
+        ["Common name"]: Common_name,
+        ["Latin name"]: Latin_name,
+      } = allName;
 
-      return {
-        "Common name": item["Common name"],
-        "Latin name": item["Latin name"],
-      };
+      return { Common_name }, { Latin_name };
 
       // return item.toLowercase().includes(query.toLowerCase());
     });
   }, [items, query]);
-
-  console.log(filterItems);
 
   // [] means to fetch data 1 time
   // [item] means to fetch data every time item changes
@@ -59,63 +54,66 @@ function Home() {
 
   return (
     <HomeDesign>
-      <div>
-        <img src="./images/cactus.png" alt="cactus" />
-        <Div>
-          <div className="search">
-            <div className="search__item">
-              <form className="form" onSubmit={handleSubmit}>
-                <input
-                  className="form__field"
-                  type="text"
-                  placeholder="Enter Plant Name Here..."
-                  value={query}
-                  onChange={(e) => setQuery(e.target.value)}
-                />
-                <button
-                  type="submit"
-                  className="btn btn--primary btn--inside uppercase"
-                >
-                  Search
-                </button>
-              </form>
-            </div>
+      <img src="./images/cactus.png" alt="cactus" />
+      <Div>
+        <div className="search">
+          <div className="search__item">
+            <form className="form" onSubmit={handleSubmit}>
+              <input
+                className="form__field"
+                type="text"
+                placeholder="Enter Plant Name Here..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <button
+                type="submit"
+                className="btn btn--primary btn--inside uppercase"
+              >
+                Search
+              </button>
+            </form>
           </div>
-        </Div>
+        </div>
+      </Div>
 
-        {filterItems.map((item) => {
-          const {
-            id,
-            Watering,
-            img,
-            Growth,
-            Use,
-            ["Light ideal"]: Light_ideal,
-            ["Common name"]: Common_name,
-            ["Latin name"]: Latin_name,
-          } = item;
-          return (
-            <Info key={id}>
-              <img src={img} alt="" />
-              <div>Plant Name: {Common_name}</div>
-              <div>Latin Name: {Latin_name}</div>
-              <div>Watering: {Watering}</div>
-              <div>Growth: {Growth}</div>
-              <div>Light Ideal: {Light_ideal}</div>
-              <div>Use: {Use}</div>
-            </Info>
+      <Div>
+        {filterData.length != 0 && (
+          <div>
+            {items.map((item) => {
+              const {
+                id,
+                Watering,
+                img,
+                Growth,
+                Use,
+                ["Light ideal"]: Light_ideal,
+                ["Common name"]: Common_name,
+                ["Latin name"]: Latin_name,
+              } = item;
+              return (
+                <Info key={id}>
+                  <img src={img} alt="" />
+                  <div>Plant Name: {Common_name}</div>
+                  <div>Latin Name: {Latin_name}</div>
+                  <div>Watering: {Watering}</div>
+                  <div>Growth: {Growth}</div>
+                  <div>Light Ideal: {Light_ideal}</div>
+                  <div>Use: {Use}</div>
+                </Info>
+                // a key property that must be put in the outermost element and must be unique.
+              );
+            })}
+          </div>
+        )}
+      </Div>
 
-            // a key property that must be put in the outermost element and must be unique.
-          );
-        })}
-
-        <Div>
-          <Button>Learn</Button>
-        </Div>
-        <Div>
-          <Button>About Us</Button>
-        </Div>
-      </div>
+      <Div>
+        <Button>Learn</Button>
+      </Div>
+      <Div>
+        <Button>About Us</Button>
+      </Div>
     </HomeDesign>
   );
 }
