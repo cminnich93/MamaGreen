2cf61f071a5f59b455e8f1a10dcf31c377756932
TK <thiratikan@gmail.com>
2022-11-27 23:58:46 -0500

    delete useMemo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { HomeDesign, Button, Div, Info } from "../styles/StyleHome";
 import { SearchPlants } from "../utils/helper/Search";
 
