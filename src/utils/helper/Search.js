fb4d778e171fe481df5739d7fd207c555f4d9d25
TK <thiratikan@gmail.com>
2022-12-16 16:34:22 -0500

    update comment

--- src/utils/helper/Search.js
+++ src/utils/helper/Search.js
@@ -2,7 +2,11 @@ export const SearchPlants = (filterItems, searchWord) => {
   if (filterItems && filterItems.length > 0) {
     // filter out plants that match the searchWord
     const newPlantArray = filterItems.filter((plant) => {
+      // form the plant obj we only take Common_name & Latin_name
       const { "Common name": Common_name, "Latin name": Latin_name } = plant;
+
+      // Create a regex object (used to filter)
+      // Make a line of regex code that finds data with searchWord in the string
       let regex = new RegExp(`W*(${searchWord})W*`, "i");
 
       // NOT NULL, null is false, null means plant with name was not found
